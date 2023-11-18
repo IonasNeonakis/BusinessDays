84ff9d9402bc3d91895b7c3acbb5557cb5070504
Ionas NEONAKIS <ionas.neonakis@gmail.com>
2023-11-18 20:56:37 +0100

    chore: add KDoc

--- src/main/kotlin/BusinessDays.kt
+++ src/main/kotlin/BusinessDays.kt
@@ -5,14 +5,46 @@ import java.time.Year
 
 private val WEEKEND_DAYS = arrayOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)
 
+/**
+ * A utility class for working with business days, taking into account weekends and given holidays.
+ *
+ * @property holidays A function that provides a collection of holidays for a given year.
+ *                   Defaults to an empty list if not specified.
+ */
 open class BusinessDays(val holidays: (year: Year) -> Collection<LocalDate> = { emptyList() }) {
+
+    /**
+     * Checks if a given date is a holiday.
+     *
+     * @param date The date to check.
+     * @return `true` if the date is a holiday, `false` otherwise.
+     */
     fun isHoliday(date: LocalDate): Boolean = date in holidays(Year.of(date.year))
 
+    /**
+     * Checks if a given date is a business day, considering both weekdays and holidays.
+     *
+     * @param date The date to check.
+     * @return `true` if the date is a business day, `false` otherwise.
+     */
     fun isBusinessDay(date: LocalDate): Boolean = isWeekday(date) && !isHoliday(date)
 
     companion object {
+
+        /**
+         * Checks if a given date is a weekend day (Saturday or Sunday).
+         *
+         * @param date The date to check.
+         * @return `true` if the date is a weekend day, `false` otherwise.
+         */
         fun isWeekendDay(date: LocalDate): Boolean = date.dayOfWeek in WEEKEND_DAYS
 
+        /**
+         * Checks if a given date is a weekday (Monday through Friday).
+         *
+         * @param date The date to check.
+         * @return `true` if the date is a weekday, `false` otherwise.
+         */
         fun isWeekday(date: LocalDate): Boolean = date.dayOfWeek !in WEEKEND_DAYS
     }
 }
--- src/main/kotlin/BusinessDaysFrance.kt
+++ src/main/kotlin/BusinessDaysFrance.kt
@@ -3,6 +3,12 @@ import java.time.Month
 import java.time.Year
 
 
+/**
+ * A specialized class for handling business days in France, taking into account French holidays.
+ *
+ * @property additionalHolidays A function that provides additional holidays for a given year.
+ *                             Defaults to an empty list if not specified.
+ */
 class BusinessDaysFrance(val additionalHolidays: (year: Year) -> Collection<LocalDate> = { emptyList() }) :
     BusinessDays({ year: Year ->
         listOf(
@@ -20,15 +26,34 @@ class BusinessDaysFrance(val additionalHolidays: (year: Year) -> Collection<Loca
         ) + additionalHolidays(year)
     }) {
 
-
     companion object {
 
+        /**
+         * Calculates the date of New Year's Eve for the given year.
+         *
+         * @param year The year for which to calculate New Year's Eve.
+         * @return The date of New Year's Eve.
+         */
         fun newYear(year: Year): LocalDate = LocalDate.of(year.value, Month.JANUARY, 1)
 
+        /**
+         * Calculates the date of Easter Monday for the given year.
+         *
+         * @param year The year for which to calculate Easter Monday.
+         * @return The date of Easter Monday.
+         */
         fun easterMonday(year: Year): LocalDate = easter(year).plusDays(1)
 
+        /**
+         * Calculates the date of Easter for the given year using the Anonymous Gregorian algorithm.
+         *
+         * The algorithm is based on the formula described in the Wikipedia article:
+         * [Date of Easter - Anonymous Gregorian algorithm](https://en.wikipedia.org/wiki/Date_of_Easter#Anonymous_Gregorian_algorithm)
+         *
+         * @param year The year for which to calculate the date of Easter.
+         * @return The date of Easter for the specified year.
+         */
         private fun easter(year: Year): LocalDate {
-            //https://en.wikipedia.org/wiki/Date_of_Easter#Anonymous_Gregorian_algorithm
             val a = year.value % 19
             val b = year.value / 100
             val c = year.value % 100
@@ -49,22 +74,76 @@ class BusinessDaysFrance(val additionalHolidays: (year: Year) -> Collection<Loca
             return LocalDate.of(year.value, n, dayOfMonth)
         }
 
+        /**
+         * Calculates the date of Labor Day (May 1st) for the given year.
+         *
+         * @param year The year for which to calculate Labor Day.
+         * @return The date of Labor Day.
+         */
         fun laborDay(year: Year): LocalDate = LocalDate.of(year.value, Month.MAY, 1)
 
+        /**
+         * Calculates the date of Victory in Europe Day (May 8th) for the given year.
+         *
+         * @param year The year for which to calculate Victory in Europe Day.
+         * @return The date of Victory in Europe Day.
+         */
         fun alliesVictory(year: Year): LocalDate = LocalDate.of(year.value, Month.MAY, 8)
 
+        /**
+         * Calculates the date of Ascension Day for the given year.
+         *
+         * @param year The year for which to calculate Ascension Day.
+         * @return The date of Ascension Day.
+         */
         fun ascension(year: Year): LocalDate = easter(year).plusDays(39)
 
+        /**
+         * Calculates the date of Pentecost for the given year.
+         *
+         * @param year The year for which to calculate Pentecost.
+         * @return The date of Pentecost.
+         */
         fun pentecost(year: Year): LocalDate = easter(year).plusDays(50)
 
+        /**
+         * Calculates the date of Bastille Day (July 14th) for the given year.
+         *
+         * @param year The year for which to calculate Bastille Day.
+         * @return The date of Bastille Day.
+         */
         fun nationalDay(year: Year): LocalDate = LocalDate.of(year.value, Month.JULY, 14)
 
+        /**
+         * Calculates the date of Assumption Day (August 15th) for the given year.
+         *
+         * @param year The year for which to calculate Assumption Day.
+         * @return The date of Assumption Day.
+         */
         fun assomption(year: Year): LocalDate = LocalDate.of(year.value, Month.AUGUST, 15)
 
+        /**
+         * Calculates the date of All Saints' Day (November 1st) for the given year.
+         *
+         * @param year The year for which to calculate All Saints' Day.
+         * @return The date of All Saints' Day.
+         */
         fun allSaintsDay(year: Year): LocalDate = LocalDate.of(year.value, Month.NOVEMBER, 1)
 
+        /**
+         * Calculates the date of Armistice Day (November 11th) for the given year.
+         *
+         * @param year The year for which to calculate Armistice Day.
+         * @return The date of Armistice Day.
+         */
         fun armistice(year: Year): LocalDate = LocalDate.of(year.value, Month.NOVEMBER, 11)
 
+        /**
+         * Calculates the date of Christmas Day (December 25th) for the given year.
+         *
+         * @param year The year for which to calculate Christmas Day.
+         * @return The date of Christmas Day.
+         */
         fun christmasDay(year: Year): LocalDate = LocalDate.of(year.value, Month.DECEMBER, 25)
     }
 }
