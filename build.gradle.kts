fee084ee35c3ff539b2d42fc50fb1adfb409fd3d
ionasNeonakis <ionas.neonakis@gmail.com>
2023-11-18 19:19:55 +0100

    chore: init project

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "fr.ionas"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("io.strikt:strikt-core:0.34.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
